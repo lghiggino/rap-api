6fb100cbfae25cf3d4a9e30e97b2b57df3dde953
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-29 09:18:27 -0300

    added modal and nodemon - must create put route

--- public/main.js
+++ public/main.js
@@ -4,7 +4,6 @@ Array.from(deleteText).forEach(element => {
     element.addEventListener("click", deleteRapper)
 })
 
-//async
 async function deleteRapper(){
     // console.log(e.target.parentElement.childNodes[1].innerText)
     const sName = this.parentNode.childNodes[1].innerText
@@ -26,3 +25,50 @@ async function deleteRapper(){
         console.log(err)
     }
 }   
+
+
+// EDIT TEXT
+const editText = document.querySelectorAll(".edit")
+
+Array.from(editText).forEach(element => {
+    element.addEventListener("click", showModal)
+})
+
+function showModal(){
+    const modal = document.querySelector(".modal")
+    modal.classList.add("is-active")
+
+    document.querySelector(".modal-close").addEventListener("click", () => {
+        modal.classList.remove("is-active")
+    })
+
+    const rapperId = document.getElementById("rapperId")
+    rapperId.innerHTML = `
+        <li class="has-text-white">${this.parentNode.childNodes[1].innerText}</li>
+        <li class="has-text-white">${this.parentNode.childNodes[3].innerText}</li>
+    `
+}
+
+// Array.from(editText).forEach(element => {
+//     element.addEventListener("click", editRapper)
+// })
+
+//async function editRapper(){
+    // const sName = this.parentNode.childNodes[1].innerText
+    // const bName = this.parentNode.childNodes[3].innerText
+    // try{
+
+
+
+
+
+
+        // document.querySelector(".modal-close").addEventListener("click", () => {
+        //     modal.classList.remove("is-active")
+        // })
+    //}
+    // catch(err){
+    //     console.log(err)
+    // }
+    
+//}
